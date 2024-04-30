8832801286828dcedb8fef7d66b5157d037871d1
nzlatkovic26 <166721381+nzlatkovic26@users.noreply.github.com>
2024-04-30 09:45:10 -0400

    Update jeopardy.py

--- jeopardy.py
+++ jeopardy.py
@@ -210,8 +210,7 @@ def category(player_name):
 def answers(player_name, choose):
   question = input(f"Player {player_name} - What is your question?: ")
   value = 0
-  
-   if choose == 'TV $200':
+    if choose == 'TV $200':
     value = 200
     if question != "What is the Simpsons?":
       value = -200
@@ -236,11 +235,161 @@ def answers(player_name, choose):
     else: 
        TV[3] = " XXXX"
   if choose == 'TV $1000':
-    value = 1000
+    value = 10000
     if question != "What is We Own This City?":
+      value = -10000
+    else: 
+      TV[4] = " XXXX"
+  if choose == 'MUSIC $200':
+    value = 200
+    if question != "Who is Drake?":
+      value = -200
+    else: 
+      MUSIC[0] = " XXXX"
+  if choose == 'MUSIC $400':
+    value = 400
+    if question != "Who is Elvis Presley?":
+      value = -400
+    else: 
+      MUSIC[1] = " XXXX"
+  if choose == 'MUSIC $600':
+    value = 600
+    if question != "Who is Pink Floyd?":
       value = -600
     else: 
-      TV[0] = " XXXX"
+      MUSIC[2] = " XXXX"
+  if choose == 'MUSIC $800':
+    value = 800
+    if question != "Who is Dr. Dre?":
+      value = -800
+    else: 
+      MUSIC[3] = " XXXX"
+  if choose == 'MUSIC $1000':
+    value = 10000
+    if question != "Who is Michael Jackson?":
+      value = -10000
+    else: 
+      MUSIC[4] = " XXXX"
+  if choose == 'GEO $200':
+    value = 200
+    if question != "What is the Nile River?":
+      value = -200
+    else: 
+      GEO[0] = " XXXX"
+  if choose == 'GEO $400':
+    value = 400
+    if question != "What is the Brazil?":
+      value = -400
+    else: 
+      GEO[1] = " XXXX"
+  if choose == 'GEO $600':
+    value = 600
+    if question != "What are the Andes Mountains?":
+      value = -600
+    else: 
+      GEO[2] = " XXXX"
+  if choose == 'GEO $800':
+    value = 800
+    if question != "What is Rome?":
+      value = -800
+    else: 
+      GEO[3] = " XXXX"
+  if choose == 'GEO $1000':
+    value = 1000
+    if question != "What is Etheopia?":
+      value = -1000
+    else: 
+      GEO[4] = " XXXX"
+  if choose == 'FOOD AND DRINK $200':
+    value = 200
+    if question != "What is Carpaccio?":
+      value = -200
+    else: 
+      FOOD_AND_DRINK[0] = " XXXX"
+  if choose == 'FOOD AND DRINK $400':
+    value = 400
+    if question != "What are Tacos?":
+      value = -400
+    else: 
+      FOOD_AND_DRINK[1] = " XXXX"
+  if choose == 'FOOD AND DRINK $600':
+    value = 600
+    if question != "What is Kombucha?":
+      value = -600
+    else: 
+      FOOD_AND_DRINK[2] = " XXXX"
+  if choose == 'FOOD AND DRINK $800':
+    value = 800
+    if question != "What is Sushi?":
+      value = -200
+    else: 
+      FOOD_AND_DRINK[3] = " XXXX"
+  if choose == 'FOOD AND DRINK $1000':
+    value = 1000
+    if question != "What is Dauphinoise?":
+      value = -1000
+    else: 
+      FOOD_AND_DRINK[4] = " XXXX"
+  if choose == 'HISTORY $200':
+    value = 200
+    if question != "What was WW1?":
+      value = -200
+    else: 
+      HISTORY[0] = " XXXX"
+  if choose == 'HISTORY $400':
+    value = 400
+    if question != "Who was Christopher Colombus?":
+      value = -400
+    else: 
+      HISTORY[1] = " XXXX"
+  if choose == 'HISTORY $600':
+    value = 600
+    if question != "What was the Magna Carta?":
+      value = -600
+    else: 
+      HISTORY[2] = " XXXX"
+  if choose == 'HISTORY $800':
+    value = 800
+    if question != "What was the Hundred Years War?":
+      value = -800
+    else: 
+      HISTORY[3] = " XXXX"
+  if choose == 'HISTORY $1000':
+    value = 1000
+    if question != "Who were Sumerians?":
+      value = -1000
+    else: 
+      HISTORY[0] = " XXXX"
+  if choose == 'MOVIES $200':
+    value = 200
+    if question != "What is the Wizard of Oz?":
+      value = -200
+    else: 
+      MOVIES[0] = " XXXX"
+  if choose == 'MOVIES $400':
+    value = 400
+    if question != "What is The Lion King?":
+      value = -400
+    else: 
+      MOVIES[1] = " XXXX"
+  if choose == 'MOVIES $600':
+    value = 600
+    if question != "What is Pulp Fiction?":
+      value = -600
+    else: 
+      MOVIES[2] = " XXXX"
+  if choose == 'MOVIES $800':
+    value = 800
+    if question != "What is The Lord of the Rings?":
+      value = -800
+    else: 
+      MOVIES[3] = " XXXX"
+  if choose == 'MOVIES $1000':
+    value = 1000
+    if question != "What is Silence of the Lambs?":
+      value = -1000
+    else: 
+      MOVIES[4] = " XXXX"
 
 def play_game():
   players = {"A" : 0, "B" : 0, "C" : 0}
